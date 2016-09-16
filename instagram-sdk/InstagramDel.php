5bd675bc9d29facd053efcab6cc353a08c06e2c8
Quang Vũ Trọng <Quang V Trng>
2016-09-17 00:43:45 +0700

    Finish instance can do send del, get, post HTTPS requets to api.instagram.com

--- instagram-sdk/InstagramDel.php
+++ instagram-sdk/InstagramDel.php
@@ -9,15 +9,19 @@ class InstagramDel extends aInstagram implements iInstagram {
 
   public final function sendHttpHeader() {
     $ch = curl_init();
-    $param = isset($this->getRawHttpHeader()['param'])?$this->getRawHttpHeader()['param']:array();
     $endpoint = isset($this->getRawHttpHeader()['endpoint'])?$this->getRawHttpHeader()['endpoint']:'';
-    $url_send = $this->getApiBaseUrl().$endpoint;
+    $url_send = $this->getApiBaseUrl().$endpoint.'?access_token='.$this->getAccessToken();
     curl_setopt($ch, CURLOPT_URL, $url_send);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POST, count($param));
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-HTTP-Method-Override: DELETE') );
+    curl_setopt($ch, CURLOPT_POSTFIELDS, array());
     $this->rawHttpResponse = json_decode(curl_exec($ch), true);
     curl_close($ch);
+  }
+
+  public final function getHttpResponse()
+  {
+      return $this->rawHttpResponse;
   }
 
 }
