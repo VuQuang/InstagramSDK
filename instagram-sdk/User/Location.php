82d086e622740f53de82549fb566b182cbf01fc2
Quang Vũ Trọng <Quang V Trng>
2016-09-19 10:45:14 +0700

    Feature get location of user, base on most check-in in feeds - Not completely.

--- instagram-sdk/User/Location.php
+++ instagram-sdk/User/Location.php
@@ -4,19 +4,34 @@ namespace Instagram\User;
 class Location {
     protected $medias;
     protected $locations = array();
+    protected $rawData;
 
     public function __construct(\Instagram\aInstagram $InstagramRequest, $endpoint_media_recent) {
         $InstagramRequest->setRawHttpHeader(array(
             'endpoint' => $endpoint_media_recent,
         ));
-        $this->medias = $InstagramRequest->sendHttpHeader()->getHttpResponse()['data'];
-        $medias = $this->medias;
-        foreach ($medias as $value)
-            array_push($this->locations, $medias['location']);
+        $this->rawData = $InstagramRequest->sendHttpHeader()->getHttpResponse();
+        $this->medias = $this->rawData['data'];
+        if(!empty($this->rawData['pagination'])) {
+            $next_url = $this->rawData['pagination']['next_url'];
+            while (!empty($next_url)) {
+                $InstagramRequest->setRawHttpHeader(array(
+                    'endpoint' => $next_url,
+                ));
+                $next_data_url = $InstagramRequest->sendHttpHeader()->getHttpResponse();
+                array_push($this->medias, $next_data_url['data']);
+                $next_url = $next_data_url['pagination']['next_url'];
+            }
+        }
+
     }
 
     public  function getMedias() {
         return $this->medias;
+    }
+
+    public function getRawData() {
+        return $this->rawData;
     }
 
     public function getLocations() {
